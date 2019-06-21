5e85549d4e8d533c5150ac53deec0b246185e6a1
Raymond Lu <raymondlu1105@qq.com>
2019-06-21 22:35:06 +0800

    Add Basic Login View

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './app.css';
-import ReactImage from './react.png';
+import LoginView from './views/LoginView';
 
 export default class App extends Component {
   state = { username: null };
@@ -16,7 +16,7 @@ export default class App extends Component {
     return (
       <div>
         {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+        <LoginView />
       </div>
     );
   }
---
+++ src/client/views/LoginView.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import '../app.css';
+
+export default class LoginView extends Component {
+  state = {};
+
+  render() {
+    return (
+      <form id="new_session" action="#" acceptCharset="UTF-8" method="post">
+        <div>
+          <input type="text" placeholder="手机号或邮箱" name="email_or_mobile_number"></input>
+        </div>
+        <div>
+          <input type="password" placeholder="密码" name="password"></input>
+        </div>
+        <div>
+          <input type="checkbox" value="true" checked="checked" name="rememberme"></input>
+          <span>记住我</span>
+        </div>
+        <button type="button" id="sign-in-form-submit-btn">
+          <span>登录</span>
+        </button>
+      </form>
+    );
+  }
+}
